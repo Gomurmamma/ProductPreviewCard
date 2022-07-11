c1818df08aa0af5b08b7be02b63ac21ad21842cc
Gomurmamma <dnaldyoung@gmail.com>
2022-07-11 17:05:42 -0400

    Base path config for deployment

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
+  basePath: "/src",
   sassOptions: {
     additionalData: `@import "./styles/_variables.scss"; @import "./styles/_mixins.scss";`,
   },
