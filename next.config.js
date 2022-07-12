53abe43e7f0387bda63cc017a4153f4a635a7ae9
Gomurmamma <dnaldyoung@gmail.com>
2022-07-11 21:02:54 -0400

    Removed src directory

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  basePath: ".next",
   sassOptions: {
     additionalData: `@import "./styles/_variables.scss"; @import "./styles/_mixins.scss";`,
   },
